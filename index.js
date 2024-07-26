34e8e97ca26adc09c6eddf78172465b7ccb3f940
paranoidy0cod3 <35105531+paranoidy0cod3@users.noreply.github.com>
2024-07-26 09:37:16 +0700

    installed downgraded  version of json-server for fix - module not found

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
+const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
+server.use(cors());
+server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-server.listen(PORT);
+server.listen(port, () => console.log("server is up!"));
